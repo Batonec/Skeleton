e8e04173bfa7ea5a5cb10880c349babf3cd9b200
batonec <pasapaval@gmail.com>
2018-01-15 02:34:36 +0300

    initial commit

---
+++ app/src/main/java/ru/batonec/skeleton/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.batonec.skeleton.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ru.batonec.skeleton.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
